7a2b7af747d3464223960a065cfb1fca627b084c
sawyer tarr <sawyertarr@outlook.com>
2023-02-22 15:11:16 -0600

    FirstCommit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>();
+
+            videoGames.Add("Fallout");
+            videoGames.Add("Skyrim");
+            videoGames.Add("Call of Duty");
+            videoGames.Add("The Witcher");
+            videoGames.Add("The Last of Us");
+            videoGames.Add("God of War");
+
+           videoGames.OrderBy(x => x).ToList().ForEach(x => Console.WriteLine(x));
+            
         }
     }
 }
